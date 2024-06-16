8af58b91fe52922f18e48df921d0ee1f858cdc6c
bourbon bowl <161977555+bourbonbowl@users.noreply.github.com>
2024-06-16 13:30:35 -0400

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bbUpdate',
-    version='1.0.3',
+    version='1.1.0',
     description='a module to update the bourbon bowl fantasy football league',
     author='bourbonbowl',
     author_email='bowlbourbon@gmail.com',
