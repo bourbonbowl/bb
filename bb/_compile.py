589fe403261c7f75a6cedc42bdaafa8bdc2bf09f
bourbonbowl <bowlbourbon@gmail.com>
2024-07-05 12:55:34 +0000

    updated completed waiers in _compile

--- bb/_compile.py
+++ bb/_compile.py
@@ -70,10 +70,11 @@ def bb_summary_output():
         else:
             transactions = json.loads(bb.config.bucket.blob('resources/data/' + bb.config.current_league_year + '/transactions/week_' + str(week) + '/transactions.json').download_as_string())
         for i in transactions:
-            for key in i['adds']:
-                if key not in bb_completed_waivers:
-                    bb_completed_waivers[key] = {}
-                    bb_completed_waivers[key].update({i['status_updated']:i['settings']['waiver_bid']})
+            if i['type']=='waiver' and i['status']=='complete':
+                for key in i['adds']:
+                    if key not in bb_completed_waivers:
+                        bb_completed_waivers[key] = {}
+                        bb_completed_waivers[key].update({i['status_updated']:i['settings']['waiver_bid']})
 
     # compile most recent pickup
     bb_recent_waiver = {}
