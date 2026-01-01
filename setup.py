064550df7bdb92b05a62fb8406bc90bc6f16176a
bourbon bowl <161977555+bourbonbowl@users.noreply.github.com>
2026-01-01 11:30:32 -0500

    Bump version from 3.0.0 to 4.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bb',
-    version='3.0.0',
+    version='4.0.0',
     description='a module to update the bourbon bowl fantasy football league',
     author='bourbonbowl',
     author_email='bowlbourbon@gmail.com',
